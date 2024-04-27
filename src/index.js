5585d994ada28c2c02dc414525c13878aeb1876a
chujiu <letchujiufly@outlook.com>
2024-04-27 11:27:44 +0800

    Movie popcorn V1

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { useState } from "react";
+// import { useState } from "react";
 import "./index.css";
 import App from "./App";
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [a, setA] = useState(0);
-  return (
-    <div>
-      <StarRating maxRating={10} size={90} onSetRating={setA} />
-      <span>this is a {a}</span>
-    </div>
-  );
-}
+// function Test() {
+//   const [a, setA] = useState(0);
+//   return (
+//     <div>
+//       <StarRating maxRating={10} size={90} onSetRating={setA} />
+//       <span>this is a {a}</span>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Test />
+    {/* <Test />
     <StarRating maxRating={20} />
     <StarRating maxRating={10} size={90} />
     <StarRating maxRating={10} size={20} />
@@ -28,6 +28,6 @@ root.render(
       size={20}
       messages={["Terrible", "Bad", "Average", "Good", "Excellent"]}
       defaultRating={3}
-    />
+    /> */}
   </React.StrictMode>
 );
